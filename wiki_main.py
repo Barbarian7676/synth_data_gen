c49f4fcd1dbc8e8115023792c2f19bc4080a0862
Atlas <153689947+Barbarian7676@users.noreply.github.com>
2024-04-23 23:17:03 -0600

    Update wiki_main.py
    Shitty code but it works

--- wiki_main.py
+++ wiki_main.py
@@ -1,19 +1,43 @@
-import os
 from api_interaction import APIInteraction
 from code_utils import CodeUtilities
 from wiki_utils import WikipediaArticleExtractor
+import random
+from first_file import check_if_valid
 
+from test import get_full_links_from_url
+url = "https://en.wikipedia.org/wiki/Wikipedia:Vital_articles/Level/5/People/Writers_and_journalists"
+try:
+    full_links = get_full_links_from_url(url)
+except Exception as e:
+    print(str(e))
+
+
+
+links = []
+for link in full_links:
+    if "en.wikipedia.org/wiki/" in link and "Category:" not in link and "User:" not in link and "File:" not in link and "Symbol:" not in link and "Wikipedia:" not in link and "Level" not in link and "Main_page" not in link and "Special:" not in link and "Portal:" not in link:
+        links.append(link)
+    else:
+        continue
+print("Links retrieved.")
+print(full_links)
 dataset_location = "instruct_dataset.jsonl"
-
-wiki_links = ["https://en.wikipedia.org/wiki/Jack_Dorsey"]
+model = "anthropic/claude-3-haiku"
+wiki_links = links
 
 
 for wiki in wiki_links:
+    print(wiki)
     article_content = WikipediaArticleExtractor.get_article_from_url(wiki)
-    top_facts = APIInteraction.generate_top_facts(article_content)
-    summaries = APIInteraction.generate_summaries(article_content)
-    CodeUtilities.write_to_dataset(article_content,top_facts,dataset_location)
-    CodeUtilities.write_to_dataset(article_content,summaries,dataset_location)
+    top_10_prompt = random.choice(["What are the top 10 facts about the preceding article?", "Give me top 10 facts about that article", "Give me a top 10", "top 10 facts", "Tell me the top 10 most interesting facts in the preceding article."])
+    summary_prompt = random.choice(["Give a summary of the preceding article.", "Summarize the preceding", "give a summary of the preceding wiki article", "summarize", "Summarize that wikipedia article", "give me a summary", "give me a recap of the preceding wiki article", "please summarize the preceding."])
+    if article_content is not "":
+        top_facts = APIInteraction.generate_top_facts(article_content, model)
+        summaries = APIInteraction.generate_summaries(article_content, model)
+        if check_if_valid(top_facts):
+            CodeUtilities.write_to_dataset(article_content + top_10_prompt,top_facts,dataset_location)
+        if check_if_valid(summaries):
+            CodeUtilities.write_to_dataset(article_content + summary_prompt,summaries,dataset_location)
 
 
 
